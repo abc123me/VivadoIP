2c580dd41869bf5491f2224a604016d2d2f6e6c5
jeremiah <jeremiahbl@protonmail.com>
2026-05-01 02:07:34 -0500

    tft_ili9341_wrapper.v: fix unconstrained internal endpoints and cleanup

--- rtl/tft_ili9341/tft_ili9341_wrapper.v
+++ rtl/tft_ili9341/tft_ili9341_wrapper.v
@@ -20,71 +20,76 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module tft_ili9341_wrapper # (
-        parameter INPUT_CLK_MHZ = 50,
-        parameter WIDTH  = 320,
-        parameter HEIGHT = 240,
-        parameter PIXEL_WIDTH = 16
-    ) (
-        // Display data out
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi scl" *)  output wire tft_sck,
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi sda" *)  output wire tft_sda,
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi dc" *)   output wire tft_dc,
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi nrst" *) output wire tft_nrst,
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi cs" *)   output wire tft_cs,
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi led" *)  output reg  tft_led,
+		parameter INPUT_CLK_MHZ = 50,
+		parameter WIDTH  = 320,
+		parameter HEIGHT = 240,
+		parameter PIXEL_WIDTH = 16
+	) (
+		// Display data out
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi scl" *)  output wire tft_sck,
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi sda" *)  output wire tft_sda,
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi dc" *)   output wire tft_dc,
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi nrst" *) output wire tft_nrst,
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi cs" *)   output wire tft_cs,
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:tftspi:1.0 m_tftspi led" *)  output reg  tft_led,
 		
 		// Pixel stream in
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream pixel_data" *)    input  wire [PIXEL_WIDTH-1:0] pixel_data,
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream core_clock" *)    input  wire core_clock,
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream core_clock_en" *) input  wire core_clock_en,
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream pixel_ready" *)   output wire pixel_ready,
-        (* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream pixel_sync" *)    output reg  pixel_sync
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream pixel_data" *)    input  wire [PIXEL_WIDTH-1:0] pixel_data,
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream core_clock" *)    input  wire core_clock,
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream core_clock_en" *) input  wire core_clock_en,
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream pixel_ready" *)   output wire pixel_ready,
+		(* X_INTERFACE_INFO = "kn4hji.ddns.net:interface:pixel_stream:1.0 s_pixel_stream pixel_sync" *)    output reg  pixel_sync
 	);
-	
+
 	localparam LAST_PIXEL = WIDTH * HEIGHT - 1;
-	
-    // Backlight is turned on after first frame is drawn
+
+	// Backlight is turned on after first frame is drawn
 	initial tft_led <= 0;
-	
+
 	// Counter for outputting a pixel sync bit
 	reg [$clog2(LAST_PIXEL):0] pixel_counter;
 	initial pixel_counter = 0;
-	
+
 	// The "pixel sync" is high whenever on the 0th pixel
 	// this allows extrernal IP to know when it can begin
 	// providing pixel data to the IP block
 	initial pixel_sync = 1;
-	
-	always @(negedge pixel_ready) begin
-	   if (pixel_counter < LAST_PIXEL) begin
-	       pixel_sync <= 0;
-	       pixel_counter <= pixel_counter + 1'b1;
-	   end else begin
-	       pixel_sync <= 1;
-	       pixel_counter <= 0;
-	       tft_led <= 1;
-	   end
+
+	reg pixel_ready_oneshot;
+	initial pixel_ready_oneshot = 0;
+	always @(negedge core_clock) begin
+		if (pixel_ready) begin
+			pixel_ready_oneshot <= 1;
+		end else if(pixel_ready_oneshot) begin
+			if (pixel_counter < LAST_PIXEL) begin
+				pixel_sync <= 0;
+				pixel_counter <= pixel_counter + 1'b1;
+			end else begin
+				pixel_sync <= 1;
+				pixel_counter <= 0;
+				tft_led <= 1;
+			end
+			pixel_ready_oneshot <= 0;
+		end
 	end
-	
+
 	// Miscellaneous signal handling
-	wire tft_rst;
 	wire tft_sdo;
-	assign tft_nrst = tft_rst; // For some reason I have to invert this
 	assign tft_sdo = 0; // The SPI transcations are one way, the MOSI is unused
-	
+
 	// ILI9341 driver IP, thanks to "thekroko" for making this!
 	// https://github.com/thekroko/ili9341_fpga/tree/master
 	tft_ili9341 # (
-        .INPUT_CLK_MHZ(INPUT_CLK_MHZ) 
-    ) (
-	   .clk(core_clock & core_clock_en),
-	   .tft_sdo(tft_sdo),
-	   .tft_sck(tft_sck),
-	   .tft_sdi(tft_sda),
-	   .tft_dc(tft_dc),
-	   .tft_reset(tft_rst),
-	   .tft_cs(tft_cs),
-	   .framebufferData(pixel_data),
-	   .framebufferClk(pixel_ready)
+		.INPUT_CLK_MHZ(INPUT_CLK_MHZ)
+	) tft (
+		.clk(core_clock & core_clock_en),
+		.tft_sdo(tft_sdo),
+		.tft_sck(tft_sck),
+		.tft_sdi(tft_sda),
+		.tft_dc(tft_dc),
+		.tft_reset(tft_nrst),
+		.tft_cs(tft_cs),
+		.framebufferData(pixel_data),
+		.framebufferClk(pixel_ready)
 	);
 endmodule
