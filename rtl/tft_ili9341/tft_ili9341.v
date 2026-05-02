56e9ec7bf1614618af7ddef0babee536872bd95e
jeremiah <jeremiahbl@protonmail.com>
2026-05-02 00:38:31 -0500

    tft_ili9341: seperate init data into its own file

--- rtl/tft_ili9341/tft_ili9341.v
+++ rtl/tft_ili9341/tft_ili9341.v
@@ -41,42 +41,16 @@ module tft_ili9341 # (
 		.tft_cs(tft_cs),
 		.idle(spi_idle));
 
-	// Init Sequence Data (based upon https://github.com/notro/fbtft/blob/master/fb_ili9341.c)
-	localparam INIT_SEQ_LEN = 52;
-	reg [5:0] init_counter;
-	reg [8:0] INIT_SEQ [0:INIT_SEQ_LEN-1];
+	reg init_clock;
+	wire [8:0] init_data;
+	wire init_done;
+	tft_ili9341_init init(
+		.clock(init_clock),
+		.reset(0),
+		.data(init_data),
+		.done(init_done));
 
-	initial begin
-		init_counter = 6'b0;
-		// Turn off Display
-		INIT_SEQ[0]  = {1'b0, 8'h28};
-		// Init (??)
-		INIT_SEQ[1]  = {1'b0, 8'hCF}; INIT_SEQ[2]  = {1'b1, 8'h00}; INIT_SEQ[3]  = {1'b1, 8'h83}; INIT_SEQ[4]  = {1'b1, 8'h30};
-		INIT_SEQ[5]  = {1'b0, 8'hED}; INIT_SEQ[6]  = {1'b1, 8'h64}; INIT_SEQ[7]  = {1'b1, 8'h03}; INIT_SEQ[8]  = {1'b1, 8'h12}; INIT_SEQ[9]  = {1'b1, 8'h81};
-		INIT_SEQ[10] = {1'b0, 8'hE8}; INIT_SEQ[11] = {1'b1, 8'h85}; INIT_SEQ[12] = {1'b1, 8'h01}; INIT_SEQ[13] = {1'b1, 8'h79};
-		INIT_SEQ[14] = {1'b0, 8'hCB}; INIT_SEQ[15] = {1'b1, 8'h39}; INIT_SEQ[16] = {1'b1, 8'h2C}; INIT_SEQ[17] = {1'b1, 8'h00}; INIT_SEQ[18] = {1'b1, 8'h34}; INIT_SEQ[19] = {1'b1, 8'h02};
-		INIT_SEQ[20] = {1'b0, 8'hF7}; INIT_SEQ[21] = {1'b1, 8'h20};
-		INIT_SEQ[22] = {1'b0, 8'hEA}; INIT_SEQ[23] = {1'b1, 8'h00}; INIT_SEQ[24] = {1'b1, 8'h00};
-		// Power Control
-		INIT_SEQ[25] = {1'b0, 8'hC0}; INIT_SEQ[26] = {1'b1, 8'h26};
-		INIT_SEQ[27] = {1'b0, 8'hC1}; INIT_SEQ[28] = {1'b1, 8'h11};
-		// VCOM
-		INIT_SEQ[29] = {1'b0, 8'hC5}; INIT_SEQ[30] = {1'b1, 8'h35}; INIT_SEQ[31] = {1'b1, 8'h3E};
-		INIT_SEQ[32] = {1'b0, 8'hC7}; INIT_SEQ[33] = {1'b1, 8'hBE};
-		// Memory Access Control
-		INIT_SEQ[34] = {1'b0, 8'h3A}; INIT_SEQ[35] = {1'b1, 8'h55};
-		// Frame Rate
-		INIT_SEQ[36] = {1'b0, 8'hB1}; INIT_SEQ[37] = {1'b1, 8'h00}; INIT_SEQ[38] = {1'b1, 8'h1B};
-		// Gamma
-		INIT_SEQ[39] = {1'b0, 8'h26}; INIT_SEQ[40] = {1'b1, 8'h01};
-		// Brightness
-		INIT_SEQ[41] = {1'b0, 8'h51}; INIT_SEQ[42] = {1'b1, 8'hFF};
-		// Display
-		INIT_SEQ[43] = {1'b0, 8'hB7}; INIT_SEQ[44] = {1'b1, 8'h07};
-		INIT_SEQ[45] = {1'b0, 8'hB6}; INIT_SEQ[46] = {1'b1, 8'h0A}; INIT_SEQ[47] = {1'b1, 8'h82}; INIT_SEQ[48] = {1'b1, 8'h27}; INIT_SEQ[49] = {1'b1, 8'h00};
-		INIT_SEQ[50] = {1'b0, 8'h29}; // Enable Display
-		INIT_SEQ[51] = {1'b0, 8'h2C}; // Start Memory-Write
-	end
+	initial init_clock = 1'b0;
 
 	// state machine with delay + idle support (used for initialization)
 	reg[23:0] delay_counter;
@@ -129,13 +103,15 @@ module tft_ili9341 # (
 
 				// setup the LCD by sending the init sequence
 				STATE_SEND_INIT_SEQ: begin
-					if (init_counter < INIT_SEQ_LEN) begin
-						spi_data <= INIT_SEQ[init_counter];
-						spi_send <= 1'b1;
-						init_counter <= init_counter + 1'b1;
-					end else begin
+					if (init_done) begin
 						state <= STATE_ASSERT_PIXEL_READY;
 						delay_counter <= DELAY_READY;
+					end else begin
+						if(!init_clock) begin
+							spi_data <= init_data;
+							spi_send <= 1'b1;
+						end
+						init_clock <= !init_clock;
 					end
 				end
 
---
+++ rtl/tft_ili9341/tft_ili9341_init.v
@@ -0,0 +1,59 @@
+`timescale 10ns / 1ns
+
+module tft_ili9341_init (
+		input  wire       reset,
+		input  wire       clock,
+		output wire       done,
+		output wire [8:0] data
+	);
+
+	// Init Sequence Data (based upon https://github.com/notro/fbtft/blob/master/fb_ili9341.c)
+	localparam INIT_SEQ_LEN = 52;
+	reg [8:0] INIT_SEQ [0:INIT_SEQ_LEN-1];
+
+	initial begin
+		// Turn off Display
+		INIT_SEQ[0]  = {1'b0, 8'h28};
+		// Init (??)
+		INIT_SEQ[1]  = {1'b0, 8'hCF}; INIT_SEQ[2]  = {1'b1, 8'h00}; INIT_SEQ[3]  = {1'b1, 8'h83}; INIT_SEQ[4]  = {1'b1, 8'h30};
+		INIT_SEQ[5]  = {1'b0, 8'hED}; INIT_SEQ[6]  = {1'b1, 8'h64}; INIT_SEQ[7]  = {1'b1, 8'h03}; INIT_SEQ[8]  = {1'b1, 8'h12}; INIT_SEQ[9]  = {1'b1, 8'h81};
+		INIT_SEQ[10] = {1'b0, 8'hE8}; INIT_SEQ[11] = {1'b1, 8'h85}; INIT_SEQ[12] = {1'b1, 8'h01}; INIT_SEQ[13] = {1'b1, 8'h79};
+		INIT_SEQ[14] = {1'b0, 8'hCB}; INIT_SEQ[15] = {1'b1, 8'h39}; INIT_SEQ[16] = {1'b1, 8'h2C}; INIT_SEQ[17] = {1'b1, 8'h00}; INIT_SEQ[18] = {1'b1, 8'h34}; INIT_SEQ[19] = {1'b1, 8'h02};
+		INIT_SEQ[20] = {1'b0, 8'hF7}; INIT_SEQ[21] = {1'b1, 8'h20};
+		INIT_SEQ[22] = {1'b0, 8'hEA}; INIT_SEQ[23] = {1'b1, 8'h00}; INIT_SEQ[24] = {1'b1, 8'h00};
+		// Power Control
+		INIT_SEQ[25] = {1'b0, 8'hC0}; INIT_SEQ[26] = {1'b1, 8'h26};
+		INIT_SEQ[27] = {1'b0, 8'hC1}; INIT_SEQ[28] = {1'b1, 8'h11};
+		// VCOM
+		INIT_SEQ[29] = {1'b0, 8'hC5}; INIT_SEQ[30] = {1'b1, 8'h35}; INIT_SEQ[31] = {1'b1, 8'h3E};
+		INIT_SEQ[32] = {1'b0, 8'hC7}; INIT_SEQ[33] = {1'b1, 8'hBE};
+		// Memory Access Control
+		INIT_SEQ[34] = {1'b0, 8'h3A}; INIT_SEQ[35] = {1'b1, 8'h55};
+		// Frame Rate
+		INIT_SEQ[36] = {1'b0, 8'hB1}; INIT_SEQ[37] = {1'b1, 8'h00}; INIT_SEQ[38] = {1'b1, 8'h1B};
+		// Gamma
+		INIT_SEQ[39] = {1'b0, 8'h26}; INIT_SEQ[40] = {1'b1, 8'h01};
+		// Brightness
+		INIT_SEQ[41] = {1'b0, 8'h51}; INIT_SEQ[42] = {1'b1, 8'hFF};
+		// Display
+		INIT_SEQ[43] = {1'b0, 8'hB7}; INIT_SEQ[44] = {1'b1, 8'h07};
+		INIT_SEQ[45] = {1'b0, 8'hB6}; INIT_SEQ[46] = {1'b1, 8'h0A}; INIT_SEQ[47] = {1'b1, 8'h82}; INIT_SEQ[48] = {1'b1, 8'h27}; INIT_SEQ[49] = {1'b1, 8'h00};
+		INIT_SEQ[50] = {1'b0, 8'h29}; // Enable Display
+		INIT_SEQ[51] = {1'b0, 8'h2C}; // Start Memory-Write
+	end
+
+	reg [5:0] counter;
+	initial counter = 6'b0;
+	assign done = counter >= INIT_SEQ_LEN;
+	assign data = INIT_SEQ[counter];
+
+	always @(posedge clock) begin
+		if (reset) begin
+			counter <= 6'b0;
+		end else begin
+			if (!done) begin
+				counter <= counter + 1'b1;
+			end
+		end
+	end
+endmodule
