b2ca1a50b9ba760919de8a43b12a132d87985671
jeremiah <jeremiahbl@protonmail.com>
2026-05-10 18:18:24 -0500

    tests/tft_ili9341/util.h: add SAFE_WHILE macro

--- tests/tft_ili9341/util.h
+++ tests/tft_ili9341/util.h
@@ -3,4 +3,12 @@
 
 #define DATA_STR(val) (val & 0xFF), (val & 0x100 ? " w/ DC" : "w/o DC")
 
+#define SAFE_WHILE(CMP, TIMES, CODE)                                \
+	cnt = 0;                                                        \
+	while(CMP) {                                                    \
+		if(cnt++ > TIMES)                                           \
+			FAIL_MSG("Infinite loop detected on line %d", __LINE__) \
+		CODE                                                        \
+	}
+
 #endif
