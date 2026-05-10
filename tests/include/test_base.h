db96beee7f160428eac5d33e8ceb351597c5ee91
jeremiah <jeremiahbl@protonmail.com>
2026-05-10 14:31:47 -0500

    tests: move test_base.h to special include folder

--- tests/include/test_base.h
+++ tests/include/test_base.h
@@ -6,6 +6,11 @@
 #include "stdio.h"
 #include "stdint.h"
 
+#define FAIL_TEST_WITH_MSG(fmt, ...) {  \
+	printf("FAIL - " fmt, __VAQ_ARGS__); \
+	FAIL_TEST                            \
+}
+
 #define FAIL_TEST err = 1; goto gtfo;
 #define CHCK_FAIL_OR_PASS if(err) goto gtfo; else puts("PASS");
 
